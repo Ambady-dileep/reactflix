80d3fd746bef1d6c33f9a8056e984646ab2bdb8e
Ambady Dileep <ambadydileep.pro@gmail.com>
2025-11-09 22:02:02 +0530

    Player set up done

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, {useEffect, useState, useParams} from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
 
 const Player = () => {
+
+    const {id} = useParams();
+
+    const [apiData, setApiData] = useState({
+        name:"",
+        key:"",
+        published_at:"",
+        typeof:""
+    })
+
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OGI1ZDEwZjc4OTE0NTM2ODg0M2U1ZWNmYzA3NGNkZiIsIm5iZiI6MTc2MjY3MTQzNy4zMDIsInN1YiI6IjY5MTAzYjRkNzAwNDhkODJlYzlhYmQ0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-L79kPiprq3pot70jtRqKFwhFUbngM6eW5bI-bFlpGo'
+        }
+        };
+    
+    useEffect(()=>{
+        fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+        .then(res => res.json())
+        .then(res => setApiData(res.results[0]))
+        .catch(err => console.error(err));
+    },[])
+
     return(
         <div className='player'>
-
+            <img src={back_arrow_icon} alt="" />
+            <iframe width='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`} title='trailer' frameBorder='0' allowFullScreen></iframe>
+            <div className="player-info">
+                <p>{apiData.published_at.slice(0,10)}</p>
+                <p>{apiData.name}</p>
+                <p>{apiData.type}</p>
+            </div>
         </div>
     )
 }
